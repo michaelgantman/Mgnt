9c2df03faedceb415cb78fa6507960ef8da081ae
michaelg <michael.gantman@cyberbitc.com>
2018-05-08 22:22:45 +0300

    Final draft before release - with forgotten updates for overview

---
+++ src/main/java/com/mgnt/lifecycle/management/example/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package with all its subpackages contains a source code example that demonstrates how infrastructure in package
+ * {@link com.mgnt.lifecycle.management} should be used. All javadocs for all classes in this package with all its
+ * sub-packages are meaningless. This package is included in utility as a source code example (that compiles and runs)
+ */
+package com.mgnt.lifecycle.management.example;
